ab1e837706185dbbea047d1f329c3a2c2a52c60f
Your Name <you@example.com>
2016-05-01 15:56:37 +0100

    finished off the required questions

--- specs/warehouse_picker_specs.rb
+++ specs/warehouse_picker_specs.rb
@@ -3,12 +3,55 @@ require_relative "../warehouse_picker_functions.rb"
 
 class TestWarehouse < Minitest::Test
 
+ # QUESTION 1
+
 def test_item_at_bay
   item = item_at_bay("b5")
   assert_equal("nail filer", item)
-
 end
 
+# # QUESTION 2
 
-
+def test_location_at_bay
+  location = location_at_bay("nail filer")
+  assert_equal("b5", location)
 end
+
+# # QUESTION 3
+
+def test_multiple_items
+  items = multiple_items(["b5", "b6", "b10"])
+  distance = multiple_items(["b5", "b6", "b10"])
+  assert_equal(["nail filer","tooth paste", "cookie jar"], items[0])
+  assert_equal(5, distance[1])
+end
+
+# # QUESTION 4
+
+# def test_multiple_bays
+#   items = multiple_bays(["shoe lace", "rusty nail", "leg warmers"])
+#   assert_equal(["c9", "c1", "c10"], items)
+# end
+
+# # def test_more_multiple_items
+# #   bays = multiple_bays
+
+
+
+# def test_more_multiple_items
+#     items = multiple_items(["b3", "c7", "c9", "a3"])
+#     # distance = multiple_items(["b3", "c7", "c9", "a3"])
+#   assert_equal(["picture frame", "paint brush", "shoe lace", "blouse"], 15)
+#   end
+
+#   def test_items_and_bays
+#       items = items_and_bays(["b3", "c7", "c9", "a3"])
+#       distance = items_and_bays(["b3", "c7", "c9", "a3"])
+#     assert_equal(["picture frame", "paint brush", "shoe lace", "blouse"], items)
+#     end
+
+
+
+
+
+  end
--- warehouse_picker_functions.rb
+++ warehouse_picker_functions.rb
@@ -1,36 +1,145 @@
-WAREHOUSE [
-{
-  a10: "rubber band",
-   a9: "glow stick",
-   a8: "model car",
-   a8: "bookmark",
-   a6: "shovel",
-   a5: "rubber duck",
-   a4: "hanger",
-   a3: "blouse",
-   a2: "stop sign",
-   a1: "needle",
-   c1: "rusty nail",
-   c2: "drill press",
-   c3: "chalk",
-   c4: "word search",
-   c5: "thermometer",
-   c6: "face wash",
-   c7: "paint brush",
-   c8: "candy wrapper",
-   c9: "shoe lace",
-  c10: "leg warmers",
-   b1: "tyre swing",
-   b2: "sharpie",
-   b3: "picture frame",
-   b4: "photo album",
-   b5: "nail filer",
-   b6: "tooth paste",
-   b7: "bath fizzers",
-   b8: "tissue box",
-   b9: "deodorant",
-  b10: "cookie jar"
-}
+
+require 'pry-byebug'
+WAREHOUSE = 
+[
+  {"a10"  => "rubber band"},
+   {"a9"  => "glow stick"},
+   {"a8"  =>  "model car"},
+   {"a7"  =>  "bookmark"},
+   {"a6"  => "shovel"},
+   {"a5"  =>  "rubber duck"},
+   {"a4"  => "hanger"},
+   {"a3"  => "blouse"},
+   {"a2"  => "stop sign"},
+   {"a1"  =>  "needle"},
+   {"c1"  => "rusty nail"},
+   {"c2"  => "drill press"},
+   {"c3"  =>  "chalk"},
+   {"c4"  =>  "word search"},
+   {"c5"  => "thermometer"},
+   {"c6"  => "face wash"},
+   {"c7"  => "paint brush"},
+   {"c8"  => "candy wrapper"},
+   {"c9"  => "shoe lace"},
+  {"c10"  => "leg warmers"},
+   {"b1"  => "tyre swing"},
+   {"b2"  => "sharpie"},
+   {"b3"  => "picture frame"},
+   {"b4"  => "photo album"},
+   {"b5"  => "nail filer"},
+   {"b6"  => "tooth paste"},
+   {"b7"  => "bath fizzers"},
+   {"b8"  => "tissue box"},
+   {"b9"  => "deodorant"},
+  {"b10"  =>  "cookie jar"}
 ]
+
+# QUESTION 1
+
+def item_at_bay(bay)
+  for items in WAREHOUSE
+    if bay == items.keys[0]
+    result = items[bay]
+    end
+  end
+return result
+end
+
+# # QUESTION 2
+
+def location_at_bay(item)
+  for items in WAREHOUSE
+    if item == items.values[0]
+    result = items.keys[0]
+    end
+  end
+return result
+end
+
+# QUESTION 3
+
+def multiple_items(bays)
+result = []
+postion = []
+  for bay in bays 
+    for item in WAREHOUSE
+      if bay == item.keys[0]
+        result.push(item.values[0])
+        postion.push(WAREHOUSE.index(item))
+          min = postion.min 
+          max = postion.max
+          distance = max - min 
+      end
+    end
+  end
+  return result, distance
+end
+
+# QUESTION 4
+
+# def multiple_bays(items)
+#   result = []
+#     for item in items 
+#       for bay in WAREHOUSE
+#         if item == bay.values[0]
+#           result.push(bay.keys[0]) 
+#         end
+#       end
+#     end
+#     return result
+#   end
+
+# def multiple_bays(items)
+# result = []
+#   for item in items 
+#     for bay in WAREHOUSE
+#       if item == bay.values[0]
+#         result.push(bay.keys[0]) 
+#       end
+#     end
+#   end
+#   return result
+# end
+
+# def tes(items)
+# postion = []
+# result = []
+#   for item in items 
+#     for bay in WAREHOUSE
+#       if item == bay.keys[0]
+#       result.push(bay.values[0])
+#         postion.push(WAREHOUSE.index(bay)) 
+#       end
+#     end
+#   end
+#   min = postion.min 
+#   max = postion.max
+#   distance = max - min
+#   return distance
+# end
+
+# def items_and_bays(items)
+#   return multiple_items(items),distance_between_bays(items)
+# end
+
+# def multiple_bays(items)
+#   result = []
+#     for item in items 
+#       for item in WAREHOUSE
+#         if bay == item.values[0]
+#           result.push(item.keys[0]) 
+#         end
+#       end
+#     end
+#     return result
+#   end
+
+
+
+
+
+
+
+
 
 
